0b262167474b6705fb29d9135277969f66c118fb
DuenoHfao <ngedric74@gmail.com>
2025-04-12 21:12:32 +0800

    fixed bug: image color not RGB

--- zero_dce/lowlight_test.py
+++ zero_dce/lowlight_test.py
@@ -1,41 +1,47 @@
 import torch
 import torchvision
 import os
+import time
+from time import localtime, strftime
+import logging
 
 import numpy as np
 from PIL import Image
 import model
-
 from lowlight_train import test_path
+
+
 
 def lowlight(image_path):
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     BASE_RESULT_DIR = './zero_dce/result'
 
-    img = Image.open(image_path)
-    img = torch.from_numpy(np.asarray(img) / 255.0).float().permute(2, 0, 1).cuda().unsqueeze(0)
+    img = Image.open(image_path).convert('RGB')
+    try:
+        img = torch.from_numpy(np.asarray(img) / 255.0).float().permute(2, 0, 1).cuda().unsqueeze(0)
     
-    net = model.enhance_net_nopool().cuda()
-    net.load_state_dict(torch.load('./zero_dce/snapshots/Epoch99.pth'))
-    net.eval()
-    print(img.shape)
-    with torch.no_grad():
-        try:
+        net = model.enhance_net_nopool().cuda()
+        net.load_state_dict(torch.load('./zero_dce/snapshots/Epoch99.pth'))
+        net.eval()
+        with torch.no_grad():
             _, enhanced, _ = net(img)
-        except RuntimeError as e:
-            print(image_path)
-            
-            print(e)
-            exit()
+    
+    except RuntimeError as e:
+        formatted_time = strftime("%a, %d %b %Y %H:%M:%S +0000", localtime())
+        logger.error(f"{formatted_time}: Error processing image {image_path}: {e}")
+        print(f"Error processing image {image_path}")
+        return
     
     dir_name = os.path.dirname(image_path.replace('\\', '/')).split('/')[-1]
     result_folder_path = os.path.join(BASE_RESULT_DIR, dir_name)
     result_path = os.path.join(result_folder_path, os.path.basename(image_path))
     os.makedirs(os.path.dirname(result_path), exist_ok=True)
     torchvision.utils.save_image(enhanced, result_path)
-    print(f"Saved enhanced image to {result_path}")
+    print(f"{result_path}") # Saved enhanced image to {result_path}
 
 if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='./zero_dce/image_test.log', level=logging.ERROR)
     test_dir = test_path
     os.makedirs(test_dir, exist_ok=True)
 
--- zero_dce/model.py
+++ zero_dce/model.py
@@ -1,9 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import math
-#import pytorch_colors as colors
-import numpy as np
 
 class enhance_net_nopool(nn.Module):
 
